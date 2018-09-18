dc567e4d85c800a6c4a875fbaf27be39db4567e8
Carson Rey <c.rey494@gmail.com>
2018-09-18 02:33:05 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,16 @@
 def hello_t(array)
-  i = 0
-  while i < array.length 
-    yield array[i]
-    i = i + 1 
+  if block_given?
+    i = 0
+    while i < array.length 
+      yield array[i]
+      i = i + 1 
+    end
+    array
+  else 
+    puts "Hey! No block was given!"
   end
-  array
 end
 
 
 # bracket method grabs what's inside it
+# You should make your methods dynamic when it comes to using 'yield'. Incorporate conditionals and give an option for when a block is not provided. 
